adc0ee32ff05fe78c6e26c82472c291a18334592
fanbarca <temirovhasan@gmail.com>
2019-07-05 19:39:40 +0000

    213123

--- src/main/java/SecondPackage/AmabiliaBot.java
+++ src/main/java/SecondPackage/AmabiliaBot.java
@@ -337,8 +337,8 @@ public class AmabiliaBot extends TelegramLongPollingBot {
                 "id SERIAL NOT NULL PRIMARY KEY,"+
                 "username varchar(225) NOT NULL UNIQUE,"+
                 "password varchar(225))";
-                String sql2 = "INSERT INTO table1"+
-                "VALUES (1, Hasan, zzzz1111*);";
+                String sql2 = "INSERT INTO table1 (username, password)"+
+                "VALUES (Hasan, zzzz1111*)";
                 String sql3 = "SELECT * FROM table1";
                 Connection conn = getConnection();
                 if (conn != null) send("Connected to the database!", message.getChatId());
