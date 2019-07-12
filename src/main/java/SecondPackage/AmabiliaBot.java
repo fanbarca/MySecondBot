85065d5f4c862beaf3636aab640941eb8791c189
fanbarca <temirovhasan@gmail.com>
2019-07-12 10:33:58 +0000

    122121

--- src/main/java/SecondPackage/AmabiliaBot.java
+++ src/main/java/SecondPackage/AmabiliaBot.java
@@ -227,7 +227,7 @@ public class AmabiliaBot extends TelegramLongPollingBot {
         }
         for (String t: showAllProducts(language)) {
             if (cb.equals(t)) {
-                edit(update.getCallbackQuery().getMessage(), t, Lan.listTypes(language).get(Integer.parseInt(sqlQuery("SELECT type from table0 where "+language+" = '"+t+"'", "type"))), Lan.goBack(language), Lan.backToMenu(language));
+                edit(t, update.getCallbackQuery().getMessage(), Lan.listTypes(language).get(Integer.parseInt(sqlQuery("SELECT type from table0 where "+language+" = '"+t+"'", "type"))), Lan.goBack(language), Lan.backToMenu(language));
             }
         }
     }
