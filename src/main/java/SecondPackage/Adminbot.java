1f06514a792ede3352c468dfbd940cc23afb4bda
fanbarca <fanbarca@users.noreply.github.com>
2019-12-07 16:08:12 +0500

    new

--- src/main/java/SecondPackage/Adminbot.java
+++ src/main/java/SecondPackage/Adminbot.java
@@ -528,7 +528,8 @@ public class Adminbot extends TelegramLongPollingBot {
         List<InlineKeyboardButton> row0 = new ArrayList<InlineKeyboardButton>();
         row0.add(new InlineKeyboardButton()
                 .setText(EmojiParser.parseToUnicode(Lan.mainMenu("Russian").get(0)))
-                .setUrl("https://t.me/"+bot.getBotUsername()+"?start=333"));
+                .setCallbackData(Lan.mainMenu("Russian").get(0))
+                .setUrl("https://t.me/"+bot.getBotUsername()));
         row0.add(new InlineKeyboardButton()
                 .setText(EmojiParser.parseToUnicode(Lan.delivery("Russian")))
                 .setCallbackData("fromChannel"+prodId)
