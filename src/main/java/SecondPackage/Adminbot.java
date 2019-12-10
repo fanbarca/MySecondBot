8203d5191b13b17d50b940f858daf7f325c97f69
Khasanjon Temirov <temirovhasan@gmail.com>
2019-12-10 04:54:54 +0000

    321

--- src/main/java/SecondPackage/Adminbot.java
+++ src/main/java/SecondPackage/Adminbot.java
@@ -584,12 +584,13 @@ public class Adminbot extends TelegramLongPollingBot {
         Bot bot = new Bot();
         String name = "<b>"+DataBase.sqlQuery("select russian from table0 where id ="+prodId, "russian")+"</b>\n";
         String description = "<i>"+DataBase.sqlQuery("select russiandescription from table0 where id ="+prodId, "russiandescription")+"</i>\n";
-        String cost =  "\n<code>"+Lan.cost("Russian")+DataBase.sqlQuery("SELECT cost from table0 where id = " + prodId, "cost")+Lan.currency("Russian") +".</code>\n";
+        String cost =  "\n<code>"+Lan.cost("Russian")+DataBase.sqlQuery("SELECT cost from table0 where id = " + prodId, "cost")+Lan.currency("Russian") +"</code>\n";
         String file_id = "";
-        String repeat = "";
+        String repeat = "_";
         for (int i = 0; i<(cost.length()-17);i++) repeat +="_";
         file_id = DataBase.sqlQuery("SELECT imageid from table0 where id ="+prodId, "imageid");
         if (file_id == null) file_id = DataBase.sqlQuery("SELECT imageid from table0 where Russian = 'Лого'", "imageid");
+        
         InlineKeyboardMarkup markup = new InlineKeyboardMarkup();
         List<List<InlineKeyboardButton>> rows = new ArrayList<List<InlineKeyboardButton>>();
         List<InlineKeyboardButton> row0 = new ArrayList<InlineKeyboardButton>();
